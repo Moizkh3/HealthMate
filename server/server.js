9001ec10f75211d3a3b15dc47b4e806f7ce49251
Moizkh3 <khatrimoiz86@gmail.com>
2026-03-03 12:53:21 +0500

    trying to  fix cors

--- server/server.js
+++ server/server.js
@@ -14,9 +14,13 @@ const port = process.env.PORT || 4000;
 // Connect to Database
 connectDB();
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-    ? process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim().replace(/\/$/, ''))
-    : ['http://localhost:5173']
+const allowedOrigins = [
+    ...(process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',').map(o => o.trim().replace(/\/$/, '')) : []),
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5174'
+].filter(Boolean);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
