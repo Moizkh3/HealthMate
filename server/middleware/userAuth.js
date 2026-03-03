d70ee9f022e82a323f15bc3f1ffc36b5c0422bb2
Moizkh3 <khatrimoiz86@gmail.com>
2026-03-03 13:03:48 +0500

    fixing

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,27 +1,36 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next) => {
+
     const { token } = req.cookies;
-    console.log("Token received in userAuth:", token ? "Yes (length: " + token.length + ")" : "No");
 
     if (!token) {
-        return res.json({ success: false, message: 'Not Authorized. Login Again' });
+
+        return res.json({ success: false, message: 'Not Authorized. Login Again' })
     }
 
     try {
+
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
 
         if (tokenDecode.id) {
+
             req.userId = tokenDecode.id;
-        } else {
-            return res.json({ success: false, message: 'Not Authorized. Login Again' });
+
+        }
+        else {
+
+            return res.json({ success: false, message: 'Not Authorized. Login Again' })
+
         }
 
         next();
 
     } catch (error) {
+
         return res.json({ success: false, message: error.message });
+
     }
-};
+}
 
 export default userAuth;
